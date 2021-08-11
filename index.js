b6c0e2b5328a8b2dea0d61e5e406e1cf6a366c79
caseytansey <ctansey12600@outlook.com>
2021-08-11 17:24:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun(activity){
+   if(typeof activity === 'string') {
+       return `This Saturday, I want to ${activity}!`
+   }else{
+       return `This Saturday, I want to roller-skate!`
+   }
+}
+
+function mondayWork(activity){
+    if(typeof activity === 'string'){
+        return `This Monday, I will ${activity}.`
+    }else{
+        return `This Monday, I will go to the office.`
+    }
+}
+
+function wrapAdjective(adjective){
+    function youAre(){
+    if (typeof adjective === 'string'){
+        return `You are ${adjective}.`
+    }else{
+          return `You are special.`
+    }
+    }
+}

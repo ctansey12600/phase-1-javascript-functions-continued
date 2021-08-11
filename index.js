a12b7bf7e67944bf758c1faf29f93e60dc565b7b
caseytansey <ctansey12600@outlook.com>
2021-08-11 18:45:34 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-function saturdayFun(activity){
-   if(typeof activity === 'string') {
-       return `This Saturday, I want to ${activity}!`
-   }else{
-       return `This Saturday, I want to roller-skate!`
-   }
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
 }
 
-function mondayWork(activity){
-    if(typeof activity === 'string'){
-        return `This Monday, I will ${activity}.`
-    }else{
-        return `This Monday, I will go to the office.`
-    }
+const mondayWork = function(activity2 = "go to the office"){
+    return `This Monday, I will ${activity2}.`
 }
 
-function wrapAdjective(adjective){
-    function youAre(){
-    if (typeof adjective === 'string'){
-        return `You are ${adjective}.`
-    }else{
-          return `You are special.`
-    }
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
     }
 }
